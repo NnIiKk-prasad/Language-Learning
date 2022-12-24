f5d74b39398464301e1b53140eebedafdb57aa24
Nikhil Prasad <nikhilprasad78630@gmail.com>
2022-12-24 14:34:01 +0530

    Added more information

--- C & C++/C programs/1. Tutorials/11.Memory_Management/DMA.c
+++ C & C++/C programs/1. Tutorials/11.Memory_Management/DMA.c
@@ -13,18 +13,31 @@ void fun()
 {
     // malloc()
     float *ptr;
-    ptr = malloc(4);
+    ptr = malloc(sizeof(float));
+    if (ptr == NULL)
+    {
+        printf("Error allocating memory\n");
+        exit(EXIT_FAILURE);
+    }
     *ptr = 4.5;
-
+    printf("Value stored in dynamically allocated memory: %f\n", *ptr);
+    
     // calloc()
     int *p;
-    p = calloc(5, 2);
+    p = calloc(3, sizeof(int));
     *(p + 0) = 34;
     *(p + 1) = 78;
+    printf("Values stored in dynamically allocated memory using calloc: %d, %d\n", *(p + 0), *(p + 1));
 
     // realloc()
     int *q;
-    q = realloc(ptr, 8);
+    q = realloc(p, 5 * sizeof(int));
+    if (q == NULL)
+    {
+        printf("Error reallocating memory\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Values stored in dynamically allocated memory using realloc: %d, %d\n", *(p + 1), *(q + 1));
 
     // free()
     free(ptr);
